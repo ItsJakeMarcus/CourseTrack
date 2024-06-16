b0598a73c611bd708ad1579f817ed351d2bd39f5
Jake Maidment <jmaidmentdev@gmail.com>
2024-06-16 21:14:19 +0100

    Initial Commit

---
+++ CourseTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CourseTrack
+//
+//  Created by Jake Maidment on 16/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CourseTrack/CourseTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseTrackApp.swift
+//  CourseTrack
+//
+//  Created by Jake Maidment on 16/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CourseTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
